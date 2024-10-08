1b537844227193c1ed3d5ea89d733ccbc35644b9
Nassir Wessin <116572504+Naisquick@users.noreply.github.com>
2024-10-08 09:16:25 -0700

    Add files via upload

---
+++ Segunda aplicacion(Variables y tipos de datos)/Segunda aplicacion/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.WriteLine("What is your name?");
+Console.WriteLine("Type your first name: ");
+string myFirstName;
+myFirstName = Console.ReadLine();
+
+string myLastName;
+Console.WriteLine("Type your last name: ");
+myLastName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+Console.ReadLine();
---
+++ Tercer proyecto(Condicional if)/Tercer proyecto(Condicional if)/Program.cs
@@ -0,0 +1,48 @@
+﻿
+/*
+Console.WriteLine("Bob's Big Giveaway");
+Console.WriteLine("Choose a door: 1, 2 or 3: ");
+string userValue = Console.ReadLine();
+
+if (userValue == "1")
+{
+    string message = "You won a new car!";
+    Console.WriteLine(message);
+}
+
+else if (userValue == "2")
+{
+    string message = "You won a new boat!";
+    Console.WriteLine(message);
+}
+else if (userValue == "3") {
+
+    string message = "You won a new cat!";
+    Console.WriteLine(message);
+}
+else
+{
+    string message = "Sorry, we didn't understand";
+    Console.WriteLine(message);
+}
+
+Console.ReadLine();
+
+*/
+
+Console.WriteLine("Bob's Big Giveaway");
+Console.Write("Choose a door: 1, 2 or 3: ");
+string userValue = Console.ReadLine();
+
+string message = (userValue == "1") ? "boat" : "strand of lint";
+
+//Console Write("You won a ");
+//Console Write(message);
+//Console Write(".");
+
+
+Console.WriteLine("You won a {0}.", message);
+
+
+Console.ReadLine();
+
