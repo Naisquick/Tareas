c98a6a23db1cf2a6096a23ea6b775df8b76c6cf5
Nassir Wessin <116572504+Naisquick@users.noreply.github.com>
2024-10-08 09:17:57 -0700

    Add files via upload

---
+++ Cuarta aplicacion(Iteracion for)/Cuarta aplicacion(Iteracion for)/Program.cs
@@ -0,0 +1,13 @@
+﻿
+for (int i = 0; i <= 10; i++)
+{
+    //Console.WriteLine(i);
+    if (i == 7)
+    {
+        Console.WriteLine("Found Seven!");
+        break;
+    }
+}
+
+Console.ReadLine();
+
